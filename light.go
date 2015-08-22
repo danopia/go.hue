ca7f05d243bf945fbb1512192e4b975cd186c411
Joshua Delsman <j@srv.im>
2015-08-22 00:57:36 -0700

    omit empty for faster marshaling

--- light.go
+++ light.go
@@ -29,23 +29,23 @@ type LightState struct {
 // SetLightState ...
 type SetLightState struct {
 	// On/Off state of the light. On=true, Off=false
-	On bool `json:"on"`
+	On bool `json:"on,omitempty"`
 
 	// The brightness value to set the light to. Brightness is a
 	// scale from 0 (the minimum the light is capable of) to 255
 	// (the maximum).
 	//
 	// NOTE: Brightness of 0 is not off.
-	Bri uint8 `json:"bri"`
+	Bri uint8 `json:"bri,omitempty"`
 
 	// The hue value to set light to. The hue value is a wrapping
 	// value between 0 and 65535. Both 0 and 65535 are red, 25500
 	// is green and 46920 is blue.
-	Hue uint16 `json:"hue"`
+	Hue uint16 `json:"hue,omitempty"`
 
 	// Saturation of the light. 255 is the most saturated (colored)
 	// and 0 is the least saturated (white).
-	Sat uint8 `json:"sat"`
+	Sat uint8 `json:"sat,omitempty"`
 
 	// The X and Y coordinates of a color in CIE color space.
 	//
@@ -54,11 +54,11 @@ type SetLightState struct {
 	//
 	// If the specified coordinates are not in the CIE color space,
 	// the closest color to the coordinates will be chosen.
-	Xy []float64 `json:"xy"`
+	Xy []float64 `json:"xy,omitempty"`
 
 	// The Mired Color temperature of the light. 2012 connected lights
 	// are capable of 153 (6500K) to 500 (2000K).
-	Ct uint16 `json:"ct"`
+	Ct uint16 `json:"ct,omitempty"`
 
 	// The alert effect, which is a temporary change to the bulb’s state,
 	// and has one of the following values:
@@ -71,23 +71,23 @@ type SetLightState struct {
 	// NOTE: This contains the last alert sent to the light and not it's
 	// current state, i.e. after the breathe cycle has finished, the bridge
 	// does not reset the alert to 'none'
-	Alert string `json:"alert"`
+	Alert string `json:"alert,omitempty"`
 
 	// The dynamic effect of the light, currently 'none' and 'colorloop' are supported.
 	// Other values will generate an error of type 7.
 	//
 	// Setting the effect to 'colorloop' will cycle through all hues using the current
 	// brightness and saturation settings.
-	Effect string `json:"effect"`
+	Effect string `json:"effect,omitempty"`
 
 	// The duration of the transition from the light's current state to the new state.
 	// This is given as a multiple of 100ms and defaults to 400ms.
 	//
 	// Example: setting to `10` will make the transition last 1 second.
-	TransitionTime uint16 `json:"transitiontime"`
+	TransitionTime uint16 `json:"transitiontime,omitempty"`
 
 	// The scene identifier if the scene you wish to recall (optional)
-	Scene string `json:"scene"`
+	Scene string `json:"scene,omitempty"`
 }
 
 // LightAttributes ...
